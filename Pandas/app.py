fa2a56f44dafc72062620c8fcf26b99fd3220c08
igorltds <igorltds@outlook.com>
2022-07-27 14:33:06 -0300

    classes novas:  System,  VendasGerentes

--- Pandas/app.py
+++ Pandas/app.py
@@ -3,70 +3,81 @@ from IPython.display import display
 import openpyxl #install
 
 # Aqui é onde a mágica acontece:
-class Vendas():
-        def __init__(self, ano):
-                self.locale = ano
-                self.vendas = { 'setembro':'',
-                                'dezembro':''}
-                self.create_df('setembro')
-                self.create_df('dezembro')
+class System():
+        def __init__(self):
+                self.df = {}
+        
+        def create_df(self):
+                self.df = pd.read_excel(self.locale)
+        def add_rows_df(self, locale):
+                self.df = pd.concat([self.df, pd.read_excel(locale)], ignore_index=True)
+        
+        def get_df(self): # retorna o DataFrame
+                return(self.df)
+        def get_head(self, length=4):
+                return self.df.head(length)
+        def get_shape(self): #retorna o tamanho do df
+                return self.df.shape # SEM args
+        def get_describe(self):
+                return self.df.describe()
+        def get_line_by_id(self, id): # returna uma linha pelo id
+                return self.df.loc[id] 
+        def get_list_line_by_id(self, list_id): # retorna um df, com linhas dos id especificos
+                return self.df.loc[list_id]
+        def get_list_line_by_order(self, beginning, end): # retorna um df de id's da ordem
+                return self.df.loc[beginning:end]
+        def get_item_specify(self, id_linha, name_coluna): # retorna um item específico
+                return self.df.loc[id_linha, name_coluna]
+        def get_df_filtered_by_column(self, # retorna um df, filtrado por uma coluna
+                                        column_name='ID Loja', 
+                                        filter_item='Norte Shopping'): 
+                return self.df.loc[self.df[column_name]==filter_item]
+        def get_columns_of_df_filtered_by_column( #retorna um colunas específicas do df, filtrado por uma coluna
+                self, filter_column=0, filter_item=0, list_columns_required=0):
+                if (filter_column==0 or filter_item==0)and list_columns_required!=0:
+                        df = self.get_df_filtered_by_column()
+                        return df.loc[list_columns_required]        
+                else:
+                        filter_item = self.df[filter_column]==filter_item
+                        return self.df.loc[filter_item, list_columns_required]
 
-        def create_df(self, mes):
-                self.vendas[mes] = pd.read_excel(f"{self.locale}Vendas_{mes}.xlsx")
-                print(f"\n{mes} criado!\n")
-        def all_display_df (self, mes, t=10):
-                match mes: # definindo mês do df
-                        case 'setembro':
-                                df = self.vendas['setembro']
-                        case 'dezembro':
-                                df = self.vendas['dezembro']
-                
-                If.title(f"  Display specs of DataFrame of {mes}: ",40, 1, 0) 
-                
-                # Começo
-                If.title("head(): ", t, 0, -1)
-                display(df.head()) # arg opcional
-                # Tamanho
-                If.title("shape: ", t, 0, -1)
-                display(df.shape) # SEM arg
-                # Descrição
-                If.title("describe(): ", t, 0, -1)
-                display(df.describe())
+        def get_specs(self):
+                return [self.get_head(2), # printa o começo, arg OPCIONAL
+                        self.get_shape(), # printa o tamanho
+                        self.get_describe()] # printa a descrição
 
-
-                If.title("loc(): ", t, 0, -1)
-
-                # Sob condição de um id
-                display(df.loc[5])
-                If.linha()
-                # Sob condição de uma lista de id's
-                display(df.loc[[0,1]]) 
-                If.linha()
-                # Sob condição de uma lista ordenada de id's
-                display(df.loc[0:5]) 
-                If.linha()
-                # item específico
-                display(df.loc[5, 'ID Loja'])
-                If.linha()
-                # Sob condição de conteudo(linha) de uma coluna
-                vendas_norte_shopping_df = df.loc[df['ID Loja']=='Norte Shopping']
-                display(vendas_norte_shopping_df) 
-                If.linha()
-                # Sob condição de conteudo de uma coluna, apenas algumas colunas retornam
-                linhas = df['ID Loja']=='Norte Shopping'
-                colunas = ['ID Loja', 'Produto', 'Quantidade']
-                vendas_norte_shopping_df =  df.loc[linhas, colunas]
-                display(vendas_norte_shopping_df)
-                If.linha()
-        def edit(self, mes):
+        def add_column_based(self, new_column, base_column):
                 # Criar coluna, com base em colunas existentes
-                vendas_df['comissão'] = vendas_df['Valor Final']*0.05
-
+                self.df[new_column] = self.df[base_column]*0.05
+        def add_column_loc (self, new_column, value):
                 # Criar nova coluna, com dados padrões (melhor desempenho parece)
-                vendas_df.loc[:, 'imposto'] = 0
-                display(vendas_df)
+                self.df.loc[:, new_column] = value
+        
+        def merge(self, main_df, second_df):
+                self.df = main_df.merge(second_df)
+        
+        def rm_lines(self, line_id):
+                self.df = self.df.drop(id, axis=0)
+        def rm_column(self, column):
+                self.df = self.df.drop(column, axis=1)
 
-# para um teste de mesa menos poluido:
+class Vendas(System):
+        def __init__(self, locale):
+                self.locale = locale
+                self.df = {}
+                self.create_df()
+                   
+class Gerentes(System):
+        def __init__(self, locale):
+                self.locale = locale
+                self.df = {}
+                self.create_df()
+
+class VendasGerentes(System):
+        def __init__(self, vendas_df, gerentes_df):
+                self.df = {}
+                self.merge(vendas_df, gerentes_df)
+                
 class Interface():
         def __init__(self): pass
 
@@ -80,11 +91,32 @@ class Interface():
                 self.linha(int(t/2), qtde2)        
 
 
-# Instanciando Vendas e Interface
-vendas_2019 = Vendas('Pandas/files/')
-If = Interface()
 
-# Printar os dois relatorios dos respectivos meses: 
-vendas_2019.all_display_df('setembro')
-vendas_2019.all_display_df('dezembro')
+def instanciar():
+        If = Interface()
+        vendas   = Vendas(  'Pandas/files/vendas.xlsx')
+        gerentes = Gerentes('Pandas/files/gerentes.xlsx')
+        vendas_gerentes = VendasGerentes(vendas.get_df(), gerentes.get_df())
+
+        print("\nClasses instanciadas: ",
+                "\nvendas:",vendas.get_shape(),
+                "\ngerentes: ",gerentes.get_shape(),
+                "\nvendas_gerentes: ",vendas_gerentes.get_shape(),
+                "\nInstanciamento ocorreu bem. \n")
+        return If, vendas_gerentes
+If , vendas = instanciar()
+
+# Adicionando dezembro
+vendas.add_rows_df('Pandas/files/2019/vendas_dezembro.xlsx') 
+print("vendas_gerentes arquivo de dezembro adcionado: ",vendas.get_shape())
+
+# Adicionando Colunas: Comissão e Imposto
+vendas.add_column_loc  ('Imposto', 0) # Adcionando coluna com valor padrão
+vendas.add_column_based('Comissão', 'Valor Final') # Adcionando coluna com base em outra
+print("vendas_gerentes adcionando as colunas 'Imposto' e 'Comissão: ",vendas.get_shape())
+
+#Removendo colunas: Comissão e Imposto
+vendas.rm_column('Comissão')
+vendas.rm_column('Imposto')
+print("vendas_gerentes removendo as colunas 'Imposto' e 'Comissão: ",vendas.get_shape())
 
