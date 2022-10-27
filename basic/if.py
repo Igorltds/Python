3ca84c6723b909d99056d10984c4ab9004e59cfe
Iltdz <Iltdz@proton.me>
2022-10-27 00:05:42 -0300

    basic python

---
+++ basic/if.py
@@ -0,0 +1,6 @@
+if False: pass
+elif False: pass
+else: print("\nif, elif and else pass")
+
+print("\nend") 
+
---
+++ basic/matriz.py
@@ -0,0 +1,22 @@
+matriz = []
+for linhas in range(4):
+    matriz.append([])
+
+
+for linha in range(len(matriz)):
+    matriz[linha].append(f"test")
+    matriz[linha].append(f"000")
+    matriz[linha].append(f"{linha}")
+
+for linha in range(len(matriz)):
+        print(matriz[linha])
+
+
+
+
+
+if False: pass
+elif False: pass
+else: print("\nif, elif and else pass")
+print("\nend") 
+
